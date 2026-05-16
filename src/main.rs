e556804664c5203a4d7298829030a7d9ef51be10
Raheem <kirlewraheem@gmail.com>
2026-05-16 14:04:01 -0500

    Added ID and flags

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ async fn main() -> io::Result<()> {
     let mut packet: Vec<u8> = Vec::new();
     let sock = UdpSocket::bind("0.0.0.0:8080").await?;
     sock.connect("8.8.8.8").await?;
-
+    //Header creation
+    // ID random
+    packet.extend_from_slice(&0x1234u16.to_be_bytes());
+    //Flags will use the one for standard query
+    packet.extend_from_slice(&0x0100u16.to_be_bytes());
     Ok(())
 }
