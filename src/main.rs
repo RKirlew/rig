c8bd501a428e19686257430af0910beaeb92e8f5
Raheem <kirlewraheem@gmail.com>
2026-05-16 16:03:49 -0500

    Added the size of the domain and reading the response, added text as tutorial.txt which tells what dig does and how it does it

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ async fn main() -> io::Result<()> {
     let mut packet: Vec<u8> = Vec::new();
     let mut buf = [0u8; 512];
     let sock = UdpSocket::bind("0.0.0.0:8080").await?;
-    sock.connect("8.8.8.8").await?;
+    sock.connect("8.8.8.8:53").await?;
     //Header creation
     // ID random
     packet.extend_from_slice(&0x1234u16.to_be_bytes());
@@ -21,5 +21,21 @@ async fn main() -> io::Result<()> {
     //ARCount
     packet.extend_from_slice(&0u16.to_be_bytes());
     //Start pushing the question
+    // Start with size of the domain name
+    packet.push(6);
+    packet.extend_from_slice(b"google");
+    //Size of the extension?
+    packet.push(3);
+    packet.extend_from_slice(b"com");
+    packet.push(0);
+
+    //QType
+    packet.extend_from_slice(&1u16.to_be_bytes());
+    //QClass
+    packet.extend_from_slice(&1u16.to_be_bytes());
+
+    sock.send(&packet).await?;
+    let n = sock.recv(&mut buf).await?;
+    println!("{:02x?}", &buf[..n]);
     Ok(())
 }
